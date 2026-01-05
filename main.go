7ea1ccaa9427773888a16913da1ef4f122d4a676
kelyan danis <kelyan.danis@ynov.com>
2026-01-05 09:24:06 +0100

    ajout authentification et contenu sur le main.go

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+
+import (
+	"log"
+	"net/http"
+	"vet-clinic-api/config"
+	"vet-clinic-api/pkg/authentification"
+	"vet-clinic-api/pkg/user"
+	"vet-clinic-api/pkg/cat"
+	"vet-clinic-api/pkg/visit"
+	"vet-clinic-api/pkg/treatment"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Routes(configuration *config.Config) *chi.Mux {
+    router := chi.NewRouter()
+	
+	// Routeurs
+	router.Mount("/api/authentification", user.Routes(configuration))
+	router.Group(func(r chi.Router) {
+    	r.Use(authentification.AuthMiddleware("demo_key"))
+	})
+
+    return router
+}
+
+func main() { 
+    // Initialisation de la configuration
+    configuration, err := config.New()
+    if err != nil {
+        log.Panicln("Configuration error:", err)
+    }
+    // Initialisation des routes
+    router := Routes(configuration)
+    log.Println("Serving on :8080")
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
--- pkg/authentication/middleware.go
+++ pkg/authentication/middleware.go
@@ -1,1 +1,34 @@
 package authentication
+
+import (
+	"context"
+	"net/http"
+) 
+
+func AuthMiddleware(secret string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				http.Error(w, "Missing token",
+				http.StatusUnauthorized)
+				return
+			}
+
+			email, err := ParseToken(secret, authHeader)
+			if err != nil {
+				http.Error(w, "Invalid token",
+				http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), "email", email)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}	
+}
+
+func GetUserFromContext(ctx context.Context) string {
+	email, _ := ctx.Value("email").(string)
+	return email
+}
